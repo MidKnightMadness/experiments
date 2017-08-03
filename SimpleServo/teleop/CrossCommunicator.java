426892b34a0d252e8aa92742016e5df1cad4f80b
glingy <gregory.ling@students.tka.org>
2017-08-03 10:12:31 -0700

    Updates and testing

---
+++ SimpleServo/teleop/CrossCommunicator.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.MiniBot.teleop;
+
+public class CrossCommunicator {
+
+    public static class Drive {
+        public static final String MOTOR_LEFT = "left";
+        public static final String MOTOR_RIGHT = "right";
+    }
+}
---
+++ SimpleServo/teleop/Servo/ServoController.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.SimpleServo.teleop.Servo;
+
+/**
+ * Created by gregory.ling on 7/28/17.
+ */
+
+public class ServoController {
+}
